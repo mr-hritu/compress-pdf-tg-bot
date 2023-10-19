83060ceb7cc0130c0e120339dc713d4f9640b8e2
mr-hritu <83459565+mr-hritu@users.noreply.github.com>
2023-10-19 09:28:01 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,38 +1,63 @@
 
 import os
-from pyrogram import Client, filters
-from PyPDF2 import PdfReader, PdfWriter
+import pyrogram
+from PyPDF2 import PdfFileWriter, PdfFileReader
 
-# Replace 'YOUR_API_ID' and 'YOUR_API_HASH' with your Telegram API credentials
-app = Client("compress_bot", api_id=29943901, api_hash="1028f4e64a5ba57ec59f4587feeabc95", bot_token="6660071929:AAFjXcut37ti05_AahCG7nvHdKRQDyUuBaI")
+# Pyrogram API credentials
+API_ID = 29943901
+API_HASH = "1028f4e64a5ba57ec59f4587feeabc95"
+BOT_TOKEN = "6660071929:AAH6JvMfr3uNEEOVkR1YTZq7c5tPrx-Jc64"
 
-# Function to handle documents
-@app.on_message(filters.document)
+# Create a Pyrogram client instance
+app = pyrogram.Client("pdf_compressor", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
+
+# Handler for /start command
+@app.on_message(pyrogram.filters.command("start"))
+def start_command(client, message):
+    client.send_message(message.chat.id, "Welcome! Send me a PDF file to compress.")
+
+# Handler for receiving documents
+@app.on_message(pyrogram.filters.document)
 def handle_document(client, message):
-    # Download the document
-    file_path = client.download_media(message)
+    # Download the file
+    file_path = client.download_media(message.document.file_id, file_name=message.document.file_name)
     
-    # Compress the document
-    compressed_file_path = compress_document(file_path)
+    # Compress the PDF file
+    compressed_file_path = compress_pdf(file_path)
     
-    # Send the compressed document back
+    # Send the compressed file back to the user
     client.send_document(message.chat.id, compressed_file_path)
     
-    # Clean up the files
+    # Delete the local files
     os.remove(file_path)
     os.remove(compressed_file_path)
 
-def compress_document(file_path):
-    # Compress the document using PyPDF2 library
-    output_path = file_path.replace(".pdf", "_compressed.pdf")
-    with open(file_path, 'rb') as input_file, open(output_path, 'wb') as output_file:
-        reader = PdfReader(input_file)
-        writer = PdfWriter()
-        for page in reader.pages:
-            writer.add_page(page)
-        writer.set_compression(True)
-        writer.write(output_file)
-    return output_path
+# Function to compress PDF file
+def compress_pdf(file_path):
+    output_file_path = f"compressed_{os.path.basename(file_path)}"
+    with open(file_path, 'rb') as file:
+        pdf_reader = PdfFileReader(file)
+        pdf_writer = PdfFileWriter()
 
-# Start the bot
+        # Copy each page and add it to the writer
+        for page_num in range(pdf_reader.getNumPages()):
+            page = pdf_reader.getPage(page_num)
+            pdf_writer.addPage(page)
+
+        # Set compression options
+        pdf_writer.setCompressionOptions({
+            'compressContentStreams': True,
+            'objectCompression': True,
+            'compressMetadata': True,
+            'imageCompress': True,
+            'imageQuality': 50  # Adjust the image quality as needed
+        })
+
+        # Write the compressed PDF file
+        with open(output_file_path, 'wb') as output_file:
+            pdf_writer.write(output_file)
+    
+    return output_file_path
+
+# Start the client
 app.run()
