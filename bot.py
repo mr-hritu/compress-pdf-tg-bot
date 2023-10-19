6421db645c2d997dfcd0483dc5e71a168b97e9c2
mr-hritu <83459565+mr-hritu@users.noreply.github.com>
2023-10-19 08:08:50 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,5 +1,7 @@
 import telebot
-from PyPDF2 import PdfReader
+from PyPDF2 import PdfReader, PdfWriter
+import tempfile
+import os
 
 # Replace 'YOUR_BOT_TOKEN' with your actual bot token
 bot = telebot.TeleBot('6660071929:AAH6JvMfr3uNEEOVkR1YTZq7c5tPrx-Jc64')
@@ -10,15 +12,20 @@ def handle_document(message):
     file_info = bot.get_file(message.document.file_id)
     downloaded_file = bot.download_file(file_info.file_path)
 
+    # Create a temporary file to save the downloaded file
+    temp_file_path = tempfile.mktemp()
+    with open(temp_file_path, 'wb') as temp_file:
+        temp_file.write(downloaded_file)
+
     # Compress the PDF file
-    compressed_file = compress_pdf(downloaded_file)
+    compressed_file = compress_pdf(temp_file_path)
 
     # Send the compressed PDF file back to the user
     bot.send_document(message.chat.id, compressed_file)
 
-def compress_pdf(file):
+def compress_pdf(file_path):
     # Create a PDF reader object
-    pdf = PdfReader(file)
+    pdf = PdfReader(file_path)
 
     # Create a PDF writer object
     pdf_writer = PdfWriter()
@@ -33,16 +40,16 @@ def compress_pdf(file):
         pdf_writer.add_page(page)
 
     # Create a new file to store the compressed PDF
-    compressed_file = open('compressed.pdf', 'wb')
+    compressed_file_path = tempfile.mktemp(suffix='.pdf')
+    with open(compressed_file_path, 'wb') as compressed_file:
+        # Write the compressed PDF to the file
+        pdf_writer.write(compressed_file)
 
-    # Write the compressed PDF to the file
-    pdf_writer.write(compressed_file)
-
-    # Close the file
-    compressed_file.close()
+    # Remove the temporary file
+    os.remove(file_path)
 
     # Return the path to the compressed PDF file
-    return 'compressed.pdf'
+    return compressed_file_path
 
 # Start the bot
 bot.polling()
